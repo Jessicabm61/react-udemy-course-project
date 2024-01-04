c3202bf684c5e0db2b6bfaaa191778d9ad794261
Jessicabm61 <jessicabeatrizmonteiro@gmail.com>
2024-01-04 06:19:11 -0300

    Trabalhando com eventos

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,15 +1,25 @@
 //Aula sobre eventos no react
 
+function renderSomenthing(x){
+    if (x) {
+       return <h1>Renderizar isso</h1> ;
+    } else {
+        return <h1>também Renderizar isso</h1>
+    }
+};
+
 const Events = () => {
 
-    const handleMyEvent = (e) => { //Utilizar o parâmetro para a função permite imprimir no console informações sobre o evento
-        console.log(e)
+    const handleMyEvent = (e) => {
     }
-
     return(
         <div>
             <div>
                 <button onClick={handleMyEvent}>Clique aqui</button>
+                <button onClick={() => console.log("clicou")}>Clique aqui também!</button>
+            </div>
+            <div>{renderSomenthing(true)}
+                 {renderSomenthing(false)}
             </div>
         </div>
     )
